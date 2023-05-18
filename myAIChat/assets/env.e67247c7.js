82c8cf40c72cf8352098a839e87d02be8826648a
zhaisd <zhaisd>
2023-05-19 06:42:59 +0800

    提交

--- myAIChat/assets/env.e67247c7.js
+++
@@ -1,1 +0,0 @@
-const t={BASE_URL:"http://127.0.0.1:8887/",BASE_WS:"ws://127.0.0.1:8887/chatWebSocket/"};export{t as e};
--- myAIChat/assets/uni-app.es.f2fdf418.js
+++
@@ -1,1 +0,0 @@
-import{K as s,L as a,O as o,M as r,N as t}from"./index-10f7d50b.js";const e=o=>(r,e=a())=>{!t&&s(o,r,e)},i=e(o),m=e(r);export{i as a,m as o};
