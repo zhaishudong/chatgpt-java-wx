710a8077a705962c841fb388a8198fe73dccc1c1
zhaisd <zhaisd>
2023-05-20 17:42:45 +0800

    提交

--- mychatgpt_server/src/main/java/com/ttpfx/controller/HealthController.java
+++ mychatgpt_server/src/main/java/com/ttpfx/controller/HealthController.java
@@ -1,9 +1,13 @@
 package com.ttpfx.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
+@CrossOrigin(origins = "*")
 public class HealthController {
 
     @GetMapping("/health")
